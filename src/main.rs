a090130154b0f63b5543a536ff3c73158b6bc349
Xander <xander@biltopia.org>
2025-07-03 00:22:10 +0200

    add some structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+enum Node {
+    Section(Section),
+    Page(Page),
+}
+
+struct FrontMatter {
+    title: String,
+}
+
+struct Section {
+    children: Vec<Node>,
+    body: Page,
+}
+
+struct Page {
+    frontmatter: FrontMatter,
+    content: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
+
+// markdown files -> parsing markdown to html -> converting to Nodes -> rendering page contents
+// using templates
